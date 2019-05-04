4c0db410f97f77c20702d6e84f7e85770fd96b94
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-04 19:40:50 +0530

    O(n) - Linear Time

---
+++ BIG-O_NOTATION/O(n).js
@@ -0,0 +1,12 @@
+// O(n) ==> Linear time --> 
+//Number of element increases is same as number of operation increases propotionally 
+ 
+const boxes = [ 1, 2, 3, 5, 6, 7];
+
+function compressAllBoxes(boxes) {
+  boxes.forEach(item => {
+    console.log(item)
+  });
+};
+
+compressAllBoxes(boxes);
