f7a16b1cae6815028d9fc88d50385daee7cd3ed3
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-04 19:51:19 +0530

    O(1) --> Constant Time

---
+++ BIG-O_NOTATION/O(1).js
@@ -0,0 +1,23 @@
+
+
+// O(1) - Constant Time 
+// Very Scalable and Excellent  in Terms of BIG-O
+// If number of element  Increaes, it doesnt effect the Operation
+
+function compressFirstBox(box) {
+    console.log(box[0]);
+}
+
+//For this function  we can have 100 elements still we will only compute  ONE operation 
+// Which is console.log the First element of the array 
+
+// ANOTHER EXAMPLE 
+
+function compressFirstBox(box) {
+    console.log(box[0]);
+    console.log(box[1]);
+    console.log(box[2]);
+}
+
+// The above function will run 3 times O(3). 
+// Even If we have 100 elements it will run only 3 Operation
--- BIG-O_NOTATION/O(n).js
+++ BIG-O_NOTATION/O(n).js
@@ -1,5 +1,5 @@
 // O(n) ==> Linear time --> 
-//Number of element increases is same as number of operation increases propotionally 
+// If Number of element increases then the  number of operation increases propotionally 
  
 const boxes = [ 1, 2, 3, 5, 6, 7];
 
