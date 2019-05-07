9e7902b5aeb060f98c3c73573b85d0d67463b124
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-07 18:32:25 +0530

    updating pointers

--- pointer.js
+++ pointer.js
@@ -7,3 +7,6 @@ object1.a = 'booys'
 
 console.log('1', object1); // booys
 console.log('2', object3); // booys
+
+// If we delete Object1 feom the memory, the object wont be full deleted as Object2 is referencing 
+// the memeory location of object1. THIS is called as GARBAGE COLLECTION  
