a998ac3cda1d34ac35e97c84cbda45ba193a0bc0
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-07 18:23:54 +0530

    pointers

---
+++ pointer.js
@@ -0,0 +1,9 @@
+
+// Pointer is just a reference '
+
+const object1 = { a: true };
+const object2 = object1;
+object1.a = 'booys'
+
+console.log('1', object1); // booys
+console.log('2', object3); // booys
